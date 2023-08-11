e2e1d1b79e721222b0e9f5c4066f070539663735
albervan.davies <albervan.davies@siagri.com.br>
2023-08-10 23:54:58 -0400

    - Ajustando mudanca da imagem com js; - Resolvendo problema da acentuacao salvando o arquivo como UTF-8

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 function toggleMode() {
   const html = document.documentElement //Todos os elementos dentro da tag html
+  html.classList.toggle("light")
 
+  //pegar a tag img
+  const img = document.querySelector("#profile img")
   if (html.classList.contains("light")) {
-    html.classList.remove("light")
+    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute(
+      "alt",
+      "Foto de Albervan para perfil com expressão sorrindo sem usar barba, com um cabdelabro ilumidando ao fundo."
+    )
   } else {
-    html.classList.add("light")
+    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute(
+      "alt",
+      "Foto de Albervan para perfil com expressão séria usando barba."
+    )
   }
+
+  //condição do ligth alterar a imagem
 }
